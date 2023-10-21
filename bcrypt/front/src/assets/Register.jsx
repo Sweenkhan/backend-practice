1e72f0d733fcc96a145263611a72fd0b983fff1f
sween <khansween@gmail.com>
2023-10-21 23:59:19 +0530

    modified req

--- bcrypt/front/src/assets/Register.jsx
+++ bcrypt/front/src/assets/Register.jsx
@@ -8,20 +8,20 @@ function Register() {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
 
-  let ujwal = "hai"
+ 
     function handleSubmit(e){
       e.preventDefault();
 
         axios.post("http://localhost:8000/register", {name, username, password})
         .then((result) =>{
-            console.log(result.data)
+            console.log(result)
         })
     }
 
 
   return (
     <div> 
-    <h1>THis Register</h1>
+    <h1>THis is Register Page.</h1>
 
     <form onSubmit={handleSubmit}>
      <input type='text' value={name} placeholder='enter your name' onChange={(e) => {setName(e.target.value)}}/>
